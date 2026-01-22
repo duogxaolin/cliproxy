4d88ec77238cc74cc2b8ffea576f11af52eb1b1c
duogxaolin <duogxaolin@gmail.com>
2026-01-22 12:36:44 +0700

    fix-layout-use-usernavbar

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-import Navbar from '../Navbar';
+import UserNavbar from './UserNavbar';
 
 interface LayoutProps {
   children: ReactNode;
@@ -8,7 +8,7 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Navbar />
+      <UserNavbar />
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         {children}
       </main>
